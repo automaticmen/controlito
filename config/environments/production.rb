c7da5ee681ed17592d0f697b04b64f5e6f8e6e9b
automaticmen <automaticmen@gmail.com>
2020-04-25 08:29:56 -0400

    Cambio el fichero production.rb para que en produccion el bootstrap funcione correctamente

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #esto lo pongo asi para que en mocha funciona bien el bootstrap.
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
